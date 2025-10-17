4bb405d8f62ba6b793d2f7354153a2c1e841f7e2
CristhianVargas060822 <r01140h@ms.upla.edu.pe>
2025-10-17 17:09:28 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 const supabaseUrl = 'https://pwptxavvhfkmyopswodw.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3cHR4YXZ2aGZrbXlvcHN3b2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMzQyMzgsImV4cCI6MjA3NTYxMDIzOH0.hCfjKOI7ViXQZMX4gm3omIGWkGDc1BGwDvmdzKuA42k';
 
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 
 
